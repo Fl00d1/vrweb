e9e0dbc7b0ed7d4c681d0a12d93bb0f471f61881
Fl00d1 <153752933+Fl00d1@users.noreply.github.com>
2023-12-14 13:37:13 +0300

    Create MotorsSpeed.ino

---
+++ MotorsSpeed.ino
@@ -0,0 +1,30 @@
+#include "MeMegaPi.h"
+
+MeMegaPiDCMotor motor1 (port2B);
+MeMegaPiDCMotor motor2 (port1B);
+
+String motorSpeed;
+String motorSpeedText1;
+String motorSpeedText2;
+int motorSpeed1;
+int motorSpeed2;
+int index;
+
+void setup()
+{
+  Serial.begin(115200);
+Serial.setTimeout(10);
+}
+
+void loop()
+{
+  if (Serial.available()){
+    motorSpeed=Serial.readString();
+    index=motorSpeed.indexOf(';');
+    motorSpeed1 = motorSpeed.substring(0,index).toInt();
+    motorSpeed2 = motorSpeed.substring(index+1,motorSpeed.length()).toInt();
+
+}
+motor1.run(motorSpeed1);
+motor2.run(-motorSpeed2);
+}
